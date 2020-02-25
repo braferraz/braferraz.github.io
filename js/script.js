7727a1ca15902670c847d1112c710c70f1742e77
Braian Ferraz <50770601+braferraz@users.noreply.github.com>
2020-02-25 06:29:04 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function botao() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+$(function() {
+    $( "#datepicker-13" ).datepicker();
+}); 
